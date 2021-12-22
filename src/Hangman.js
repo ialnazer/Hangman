c34916d44179abf5af86a24247d36ddc6c13bd8f
Israa Alnazer <israa_alnazer@hotmail.com>
2021-12-22 18:16:54 +0200

    add image alt

--- src/Hangman.js
+++ src/Hangman.js
@@ -62,7 +62,7 @@ class Hangman extends Component {
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
-        <img src={this.props.images[this.state.nWrong]} />
+        <img src={this.props.images[this.state.nWrong]} alt={`${this.state.nWrong}/${this.props.maxWrong}`}/>
         <p >Number wrong: {this.state.nWrong}</p>
         <p className='Hangman-word'>
           {!gameOver
